5b70e059a64bb52398c1781d426f1c4b9836cb21
Michael Krasnov <mihalKrasnov@gmail.com>
2019-09-28 18:12:33 -0400

    HMR for development

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = (app) => ReactDOM.render(app, document.getElementById('root'));
+
+render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(<NextApp />);
+  });
+}
--- src/store.js
+++ src/store.js
@@ -8,8 +8,9 @@ import logger from 'redux-logger';
 export const history = createBrowserHistory();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reducer = createRootReducer(history);
 const store = createStore(
-  createRootReducer(history),
+  reducer,
   undefined,
   composeEnhancers(
     applyMiddleware(
@@ -19,4 +20,13 @@ const store = createStore(
     )
   )
 );
+
+if (process.env.NODE_ENV !== 'production') {
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      store.replaceReducer(reducer);
+    });
+  }
+}
+
 export default store;
