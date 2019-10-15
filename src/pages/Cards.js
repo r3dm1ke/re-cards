75de540620d41721910b13697dba1c13f28d5c19
Michael Krasnov <mihalKrasnov@gmail.com>
2019-10-15 19:28:05 -0400

    Refactored cards page to functional and added title to said page

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import {withStyles, Divider, Grow} from '@material-ui/core';
-import {connect} from 'react-redux';
+import React from 'react';
+import {Grow, makeStyles, Typography} from '@material-ui/core';
+import {useSelector, useDispatch} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 import CardThumbnail from '../components/cards/CardThumbnail';
 import Filters from '../components/cards/Filters';
 import {
-  deck_selected, delete_card,
+  delete_card,
 } from '../actions/cards/cards';
 import {
   open_edit_card_dialog_for_existing_card,
@@ -14,14 +14,33 @@ import {
 import NewItemCard from '../components/common/NewItemCard';
 import EditCardDialog from '../components/cards/edit_dialog/EditCardDialog';
 
-class CardsPage extends Component {
-  renderCards() {
-    console.log('now rendering these cards: ');
-    console.dir(this.props.cards);
-    if (this.props.cards === undefined) {
-      return this.renderSkeletons();
+const useStyles = makeStyles((theme) => ({
+  cardContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  title: {
+    textAlign: 'center',
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(4),
+  },
+}));
+
+export default () => {
+  const logged_in = useSelector((state) => state.auth.logged_in);
+  if (!logged_in) return <Redirect to={'/'} />;
+
+  const cards = useSelector((state) => state.cards.filtered_cards);
+  // eslint-disable-next-line no-unused-vars
+  const _ = useSelector((state) => state.cards.refresh_helper);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const render_cards = () => {
+    if (cards === undefined) {
+      return render_skeletons();
     }
-    return this.props.cards
+    return cards
       .map((card, index) => (
         <Grow timeout={index * 250} key={card.id} in>
           <CardThumbnail
@@ -30,62 +49,32 @@ class CardsPage extends Component {
             answer={card.answer}
             deckName={card.deck_name}
             ratio={card.ratio}
-            onEdit={() => this.props.edit_existing_card(card)}
-            onDelete={() => this.props.delete_card(card)}
+            onEdit={() => dispatch(open_edit_card_dialog_for_existing_card(card))}
+            onDelete={() => dispatch(delete_card(card))}
           />
         </Grow>
       ));
-  }
+  };
 
-  renderSkeletons() {
-    const ids = [1, 2, 3, 4, 5];
-    return ids.map((id) => (
+  const render_skeletons = () => {
+    return [1, 2, 3, 4, 5].map((id) => (
       <CardThumbnail key={id} skeleton />
     ));
-  }
+  };
 
-  handleDeckChange(event) {
-    this.props.deck_selected(event.target.value);
-  }
+  const final_delay = cards === undefined ? 0 : cards.length * 250;
 
-  render() {
-    const {logged_in, classes, open_new_card, cards} = this.props;
-    const finalDelay = cards === undefined ? 0 : cards.length * 250;
-
-    if (!logged_in) return <Redirect to={'/'} />;
-    return (
-      <div>
-        <Filters />
-        <div className={classes.cardContainer}>
-          {this.renderCards()}
-          <Grow timeout={finalDelay} in>
-            <NewItemCard onClick={open_new_card}/>
-          </Grow>
-        </div>
-        <EditCardDialog />
+  return (
+    <div>
+      <Typography variant={'h2'} className={classes.title}>Your cards</Typography>
+      <Filters />
+      <div className={classes.cardContainer}>
+        {render_cards()}
+        <Grow timeout={final_delay} in>
+          <NewItemCard onClick={() => dispatch(open_edit_card_dialog_for_new_card())}/>
+        </Grow>
       </div>
-    );
-  }
-}
-
-const styles = (theme) => ({
-  cardContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-
-});
-const mapStateToProps = (state) => ({
-  logged_in: state.auth.logged_in,
-  cards: state.cards.filtered_cards,
-  selected_deck: state.cards.selected_deck,
-  decks: state.cards.decks,
-  _: state.cards.refresh_helper,
-});
-const mapDispatchToProps = (dispatch) => ({
-  deck_selected: (deck) => dispatch(deck_selected(deck)),
-  open_new_card: () => dispatch(open_edit_card_dialog_for_new_card()),
-  edit_existing_card: (card) => dispatch(open_edit_card_dialog_for_existing_card(card)),
-  delete_card: (card) => dispatch(delete_card(card)),
-});
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(CardsPage));
+      <EditCardDialog />
+    </div>
+  );
+};
