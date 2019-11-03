8346bceba0011af408f4a6c01b6f8daea4722abe
Michael Krasnov <mihalKrasnov@gmail.com>
2019-11-03 10:40:37 -0500

    Consistent ui for creating cards & decks

--- src/components/common/CreateNewFab.js
+++ src/components/common/CreateNewFab.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Fab, makeStyles} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
-import {useDispatch} from 'react-redux';
-import {open_edit_card_dialog_for_new_card} from '../../actions/cards/cards_form';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,9 +16,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default () => {
-  const dispatch = useDispatch();
-  const on_click = () => dispatch(open_edit_card_dialog_for_new_card());
+export default (props) => {
+  const {on_click, label} = props;
   const classes = useStyles();
 
   return (
@@ -31,7 +28,7 @@ export default () => {
       onClick={on_click}
       size={'large'}
     >
-      <AddIcon />Create new
+      <AddIcon />{label}
     </Fab>
   );
 };
